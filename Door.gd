24f30cd00e23afa8c1bfdd61b7459feae92c2a22
David Black <david_black_uk@yahoo.com>
2021-02-27 11:23:16 +0000

    First check in

---
+++ Door.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+export(String, FILE, "*.tscn") var next_level
+
+#
+# Fred has walked into the door and triggered entry to the next level
+#
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene(next_level) # Replace with function body.
+
+#
+# Animate the shutter then when complete, remove the static body 
+# collision that prevents pred from entering and hitting the Area2d
+# that initiates the level change
+#
+func _on_Player_level_complete():
+	var animator = get_node("Area2D/AnimationPlayer")
+	animator.play("Roller")
+	yield(animator, "animation_finished")
+	get_node("StaticBody2D").set_collision_mask_bit(0, false)
---
+++ Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+const PLATFORM_COLLISION_BIT = 1
+const HORIZONTAL_VELOCITY = 150
+const JUMP_VELOCITY = 320
+const GRAVITY = 10
+
+var motion = Vector2()
+var is_jumping = false
+
+signal level_complete()
+
+
+func _physics_process(delta):
+
+	process_input()
+				
+	motion = move_and_slide(motion, Vector2.UP)
+
+func process_input():
+	motion.y += GRAVITY
+	
+	var animation_player = get_node("AnimationPlayer")
+	var sprite = get_node("Sprite")
+	
+	if Input.is_action_pressed("ui_left"):
+		motion.x = -HORIZONTAL_VELOCITY
+		animation_player.play("Walk")
+		sprite.set_flip_h(true)
+
+	elif Input.is_action_pressed("ui_right"):
+		animation_player.play("Walk")
+		sprite.set_flip_h(false)		
+		motion.x = HORIZONTAL_VELOCITY
+	else:
+		motion.x = 0
+		get_node("AnimationPlayer").play("Idle")
+		
+	if Input.is_action_just_pressed("ui_jump") and is_on_floor():
+		motion.y = -JUMP_VELOCITY
+		is_jumping = true
+
+	if Input.is_action_just_pressed("ui_home"):
+		emit_signal("level_complete")

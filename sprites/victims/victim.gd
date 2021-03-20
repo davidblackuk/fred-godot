7985a74d93e4edc00d98c290222df09f64cf8c6b
David Black <david_black_uk@yahoo.com>
2021-03-20 13:10:36 +0000

    Adding statemachine

--- sprites/victims/victim.gd
+++ sprites/victims/victim.gd
@@ -2,20 +2,27 @@ extends "res://sprites/SpriteBase.gd"
 
 signal victim_rescued()
 
-enum VictimState { WAITING, RESCUED }
-
-var state = VictimState.WAITING
 const points_scored = 120
 
+onready var animation_player = get_node("Area2D/AnimationPlayer")
+onready var audio_player = get_node("Area2D/AudioStreamPlayer")
+onready var state_machine = get_node("StateMachine")
+
+var _has_intersectedPlayer = false
 
 func _on_Area2D_body_entered(body):
-	if state == VictimState.WAITING && body.name  == "Player":
-		var animation_player = get_node("Area2D/AnimationPlayer")
-		var audio_player = get_node("Area2D/AudioStreamPlayer")
-		GameState.add_score(points_scored)
-		emit_signal("victim_rescued")
-		animation_player.play("Rescued")
-		audio_player.play()
-		state = VictimState.RESCUED
-		yield(animation_player, "animation_finished")	
-		queue_free()
+	if  body.name  == "Player":
+		_has_intersectedPlayer = true				
+		
+func is_rescued():
+	return _has_intersectedPlayer
+
+
+
+func show_rescued():
+	GameState.add_score(points_scored)
+	emit_signal("victim_rescued")
+	animation_player.play("Rescued")
+	audio_player.play()
+	yield(animation_player, "animation_finished")	
+	queue_free()

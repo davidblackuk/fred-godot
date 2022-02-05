88455959d3e59b08386d3e3870e6dfe6c13f19c1
David Black <david_black_uk@yahoo.com>
2022-02-05 13:28:57 +0000

    Screwed up animations trying to fix

--- controls/moving-platforms/MovingPlatform.gd
+++ controls/moving-platforms/MovingPlatform.gd
@@ -2,11 +2,13 @@ tool
 extends Node2D
 
 
-export(int) var image_frame = 0
+export var image_frame = 0 setget _set_image_frame
 
 onready var platform_sprite = get_node("Platform/Sprite")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	platform_sprite.frame = image_frame
+	_set_image_frame(image_frame)
 
+func _set_image_frame(value):
+	platform_sprite.frame = value

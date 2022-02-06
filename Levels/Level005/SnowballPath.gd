14b85cafbf4caf2075e3c0f6c4dd2a82c93718c5
David Black <david_black_uk@yahoo.com>
2022-02-06 16:15:07 +0000

    Ice level is complete

--- Levels/Level005/SnowballPath.gd
+++ Levels/Level005/SnowballPath.gd
@@ -16,10 +16,9 @@ func _ready():
 # flips the sprite at the halfway point
 func _process(delta):
 	var follower = get_node("./PathFollow2D")
-	var sprite = get_node("./PathFollow2D/Area2D/AnimatedSprite")
-	var new_offset = follower.get_offset() + 100 * delta
+	var new_offset = follower.get_offset() + 250 * delta
 	follower.set_offset(new_offset)
-	sprite.flip_h = (follower.unit_offset < 0.5)
+	
 
 
 func _on_Area2D_body_entered(body):
